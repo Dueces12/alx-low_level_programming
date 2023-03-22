0fae4e7f3178631e6d9ba4b0afb54c1898cf7d15
Dueces12 <snydersderick37@gmail.com>
2023-03-22 14:23:17 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int);
+int print_last-digit(int);
+int print_sign(int n);
+int times_table(void);
+int add(int,int);
+int jack_bauer(void);
+int print_times_table(int n);
+int print_to_98(int n);
+
+#endif
